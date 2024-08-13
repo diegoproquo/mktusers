f4cb3cff08f80641671548a2b524c5447cd0f7ce
ProquoDiego <diego@proquo.es>
2024-08-13 14:58:42 +0200

    [FIX] Quitar la chart demo que no servia para nada

--- application/views/plantillas/footer.php
+++ application/views/plantillas/footer.php
@@ -36,8 +36,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
 <script src="<?= base_url() ?>public/js/scripts.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-<script src="<?= base_url() ?>public/assets/demo/chart-area-demo.js"></script>
-<script src="<?= base_url() ?>public/assets/demo/chart-bar-demo.js"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
 <script src="<?= base_url() ?>public/js/datatables-simple-demo.js"></script>
 
