87e46023f7e4579b706943a3841967a399868a8c
ProquoDiego <diego@proquo.es>
2024-08-14 13:04:02 +0200

    [NEW] Mejoras de diseño en la pagina de login

--- application/views/login/show.php
+++ application/views/login/show.php
@@ -1,5 +1,5 @@
 
-<div class="wrapper_login">
+<div class="wrapper_login" style="text-align:center">
 
     <form class="form-signin" method="post" action="Login/iniciarSesion" onsubmit="return loading()">
 
@@ -7,17 +7,32 @@
         <input type="text" class="form-control" name="user" placeholder="Usuario" required="" />
         <input type="password" class="form-control" name="pass" placeholder="Contraseña" required="" />
 
-        <button class="btn btn-lg btn-primary btn-block" type="submit" id="btnLogin">Login</button>
+        <button class="btn btn-lg btn-primary btn-block mb-2" type="submit" id="btnLogin">Login</button>
 
         <?php if ($this->session->flashdata('error')) : ?>
-            <div class="alert alert-danger mt-4" role="alert">
+            <div class="alert alert-danger" role="alert">
                 <?php echo $this->session->flashdata('error'); ?>
             </div>
         <?php endif; ?>
 
     </form>
+
+    <a class="custom">Powered by <img src="assets/img/logo_proquo.png" width="55" ></a>
+
 </div>
 
+<style>
+.custom {
+    font-family: Arial, sans-serif;
+    font-size: 14px;
+    color: #555;
+    display: block;
+    margin-top: 20px;
+    text-decoration: none;
+    text-align: center;
+}
+
+    </style>
 <script>
     function loading() {
         var botonLogin = document.getElementById("btnLogin");
