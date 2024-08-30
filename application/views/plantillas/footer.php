910fd90788ec981a7769574c3f293a6de63eff14
ProquoDiego <diego@proquo.es>
2024-08-30 10:38:03 +0200

    [NEW] Actualizar librerias del footer para que funcionen en local

--- application/views/plantillas/footer.php
+++ application/views/plantillas/footer.php
@@ -13,32 +13,10 @@
     </div>
 </footer>
 
-<!-- Bootstrap core JavaScript-->
-<script src="<?= base_url() ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-<!-- Core plugin JavaScript-->
-<script src="<?= base_url() ?>vendor/jquery-easing/jquery.easing.min.js"></script>
-
-<!-- Custom scripts for all pages-->
-<script src="<?= base_url() ?>js/sb-admin-2.min.js"></script>
-
-<!-- Page level plugins -->
-<script src="<?= base_url() ?>vendor/chart.js/Chart.min.js"></script>
-
-<!-- Page level custom scripts 
-<script src="<?= base_url() ?>js/demo/chart-area-demo.js"></script>
-<script src="<?= base_url() ?>js/demo/chart-pie-demo.js"></script>
--->
-
-
-
-<script src="<?= base_url() ?>js/popper.min.js"></script>
-<script src="<?= base_url() ?>js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 <script src="<?= base_url() ?>public/js/scripts.js"></script>
-<script src="<?= base_url() ?>js/Chart.min.js" crossorigin="anonymous"></script>
-
-<script src="<?= base_url() ?>js/simple-datatables.min.js" crossorigin="anonymous"></script>
-<script src="<?= base_url() ?>public/js/datatables-simple-demo.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
 
 </body>
 
