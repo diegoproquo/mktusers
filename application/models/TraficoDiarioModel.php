e428529f471a390390a6f2de4f95987318fad2ee
ProquoDiego <diego@proquo.es>
2024-09-27 18:31:25 +0200

    [NEW] Controlador y modelo de tbl_trafico_diario. Metodo para registrar el trafrico consumido por el usuario durante la sesion al hacer logout

---
+++ application/models/TraficoDiarioModel.php
@@ -0,0 +1,81 @@
+<?php
+
+class TraficoDiarioModel extends CI_Model
+{
+    public $id;
+    public $fecha;
+    public $bytes_descarga;
+    public $bytes_carga;
+
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    public function registrarTraficoSesion($bytes_descarga, $bytes_carga)
+    {
+        // Obtener la fecha actual
+        $fecha_actual = date('Y-m-d');
+
+        // Verificar si ya existe un registro para la fecha actual
+        $query = $this->db->get_where('tbl_trafico_diario', array('fecha' => $fecha_actual));
+        $registro = $query->row();
+
+        if ($registro) {
+            // Si existe el registro, añadimos los bytes a los existentes
+            $this->db->set('BYTES_DESCARGA', 'BYTES_DESCARGA + ' . (int)$bytes_descarga, FALSE);
+            $this->db->set('BYTES_CARGA', 'BYTES_CARGA + ' . (int)$bytes_carga, FALSE);
+            $this->db->where('FECHA', $fecha_actual);
+            $this->db->update('tbl_trafico_diario');
+        } else {
+            // Si no existe, crear un nuevo registro con 1 conexión
+            $data = array(
+                'FECHA' => $fecha_actual,
+                'BYTES_DESCARGA' => (int)$bytes_descarga,
+                'BYTES_CARGA' => (int)$bytes_carga
+            );
+            $this->db->insert('tbl_trafico_diario', $data);
+        }
+    }
+
+    // Devuelve un array con los registros de las conexiones de los ultimos 7 dias agrupados por fecha ascendente
+    public function getTrafico7dias()
+    {
+        $traficoDescarga = array_fill(0, 8, 0);
+        $traficoCarga = array_fill(0, 8, 0);
+
+        // Consulta para obtener las conexiones sumadas por fecha
+        $this->db->select("DATE_FORMAT(FECHA, '%d/%m') as fecha_formateada, BYTES_DESCARGA, BYTES_CARGA");
+        $this->db->order_by('FECHA', 'DESC');
+        $this->db->limit(7);
+
+        $query = $this->db->get("tbl_trafico_diario");
+        $resultados = $query->result();
+
+        // Crear un mapa de fechas para actualizar las conexiones
+        $fecha_map = [];
+        for ($i = 0; $i < 8; $i++) {
+            $fecha_map[date('d/m', strtotime("-$i day"))] = $i;
+        }
+
+        // Rellenamos los valores del array con los resultados de la consulta
+        foreach ($resultados as $item) {
+            $key = $fecha_map[$item->fecha_formateada] ?? null;
+            if ($key !== null) {
+
+                // Conertimos Bytes a MB
+                $bytes_out = round($item->BYTES_DESCARGA * 0.000001, 2);
+                $traficoDescarga[$key] = $bytes_out;
+
+                $bytes_in = round($item->BYTES_CARGA * 0.000001, 2);
+                $traficoCarga[$key] = $bytes_in;
+            }
+        }
+
+        $traficoDescarga = array_reverse($traficoDescarga);
+        $traficoCarga = array_reverse($traficoCarga);
+
+        // Invertimos el array para que las fechas más recientes estén al final (opcional)
+        return array($traficoDescarga, $traficoCarga);
+    }
+}
