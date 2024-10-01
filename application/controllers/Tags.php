354f1cda996f654fddb08e78f6d633ea4e70ed46
ProquoDiego <diego@proquo.es>
2024-10-01 13:46:15 +0200

    [NEW] Vista funcional de tags con drag and drop. Añadir, editar y eliminar tags correctamente

--- application/controllers/Tags.php
+++ application/controllers/Tags.php
@@ -12,6 +12,7 @@ class Tags extends CI_Controller
 		$this->load->helper('url');
 		$this->load->helper('html');
 		$this->load->model('TagsModel');
+        $this->load->model('ObtenerUltimoIdModel');
 
 		if (!$this->session->userdata('logged_in')) {
 			redirect(base_url() . "Login");
@@ -21,13 +22,8 @@ class Tags extends CI_Controller
 
 	public function show()
 	{
-		$columna1 = "ID";
-		$columna2 = "Nombre";
-		$columna3 = "Color";
-		$columna4 = "Deleted_At";
 
-		$data['columns'] = array($columna1, $columna2, $columna3, $columna4);
-		$data['data'] = $this->MostrarRecargarDatosTags();
+		$data['tags'] = $this->TagsModel->getTags();
 
 		$this->load->view('plantillas/header');
 		$this->load->view('tags/show', $data);
@@ -41,7 +37,7 @@ class Tags extends CI_Controller
 
         //Nuevo
         if ($datos['id'] == -1) {
-            $id = $this->ObtenerUltimoIdModel->obtenerUltimoId('tbl_usuarios_web');
+            $id = $this->ObtenerUltimoIdModel->obtenerUltimoId('tbl_tags');
             $this->TagsModel->nuevoTag($id, $datos['nombre'], $datos['color']);
         }
 
@@ -50,34 +46,21 @@ class Tags extends CI_Controller
             $this->TagsModel->guardarCambios($datos['id'], $datos['nombre'], $datos['color'], null);
         }
 
-        $data = $this->MostrarRecargarDatosTags();
+        $data = $this->TagsModel->getTags();
 
         echo json_encode(array(true, $data));
     }
 
     public function EliminarTag()
     {
-		$input = file_get_contents('php://input');
-		$decodedInput = json_decode($input, true);
 
-		$datos = $decodedInput['usuariosweb'];
+        $datos = $this->input->post('datos');
 
-        foreach($datos as $item) $this->TagsModel->eliminar($item['ID']);
+        $this->TagsModel->eliminar($datos['id']);
 
-        $data = $this->MostrarRecargarDatos();
+        $data = $this->TagsModel->getTags();
+
         echo json_encode(array(true, $data));
-    }
-
-    function MostrarRecargarDatosTags()
-    {
-
-        $tags = $this->TagsModel->getTags();
-
-        foreach ($tags as $item) {
-
-        }
-
-        return $tags;
     }
 
 }
